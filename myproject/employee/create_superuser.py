332f477ccf5c2c173286945094279820c80afe10
santhosh1313 <santhoshkumar131310@gmail.com>
2026-07-04 00:52:32 +0530

    Create render admin user

--- myproject/employee/create_superuser.py
+++ myproject/employee/create_superuser.py
@@ -10,15 +10,20 @@ django.setup()
 
 from django.contrib.auth.models import User
 
+
 username = "Santhosh"
 password = "Santhosh#53"
 
+
 if not User.objects.filter(username=username).exists():
+
     User.objects.create_superuser(
         username=username,
-        email="admin@gmail.com",
+        email="santhosh@gmail.com",
         password=password
     )
-    print("Superuser created")
+
+    print("Superuser created successfully")
+
 else:
     print("Superuser already exists")
